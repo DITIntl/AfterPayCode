afe5dbc4d90a2b0e357abe914fc87b20b4a32934
Randy Wijerathne <randy@router.id.au>
2019-09-17 23:17:08 +1000

    SSH key pair generation

--- AWSIaC.py
+++ AWSIaC.py
@@ -46,7 +46,20 @@ response = client.describe_images(
 image_details = sorted(response['Images'],key=itemgetter('CreationDate'),reverse=True)
 ami_id = image_details[0]['ImageId']
 logging.basicConfig(format='%(asctime)s %(message)s')
+
 logging.warning('Script will use Amazon AMI ID :  %s for bulding base customized AMI', ami_id)
+
+
+#ec2 = boto3.client('ec2')
+#response = ec2.create_key_pair(KeyName='AfterPayKey2')
+logging.warning('Generating a SSH-key file AfterPayKey.pem whcih can be used to loggin to server ')
+
+#f= open("AfterPayKey2.pem","w+")
+#f.write(str(response))
+#f.close()
+
+logging.warning('Creating a customised AMI ...')
+
 ec2_client = boto3.client('ec2')
 response = ec2_client.run_instances(ImageId=ami_id,
                                             InstanceType='t2.micro',
