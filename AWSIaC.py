3b85a1881ac7c85063c1c6cc75ddeb825b9efbf1
Randy Wijerathne <randy@router.id.au>
2019-09-17 21:42:05 +1000

    Create a EC2 using Amazon standard AMI
    Create a EC2 using Amazon standard AMI

--- AWSIaC.py
+++ AWSIaC.py
@@ -1,1 +1,31 @@
+import logging
 import boto3
+from operator import itemgetter
+
+"""Exercise create_ec2_instance()"""
+client = boto3.client('ec2')
+response = client.describe_images(
+Filters=[
+        {
+            'Name': 'description',
+            'Values': [
+                'Amazon Linux AMI*',
+            ]
+        },
+ ],
+ Owners=[
+    'amazon'
+ ]
+)
+# Sort on Creation date Desc
+image_details = sorted(response['Images'],key=itemgetter('CreationDate'),reverse=True)
+ami_id = image_details[0]['ImageId']
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.warning('Script will use Amazon AMI ID :  %s for bulding base customized AMI', ami_id)
+ec2_client = boto3.client('ec2')
+response = ec2_client.run_instances(ImageId=ami_id,
+                                            InstanceType='t2.micro',
+                                            KeyName='AfterPayKey',
+                                            MinCount=1,
+                                            MaxCount=1)
+    
